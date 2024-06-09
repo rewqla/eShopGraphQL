148aebdf39b82af22cccbf6a588cc96f73f81119
rewqla <desares526@gmail.com>
2024-06-09 16:46:14 +0300

    Add a midleware extension

---
+++ src/Catalog.API/Types/UseToUpper.cs
@@ -0,0 +1,27 @@
+﻿using HotChocolate.Types.Descriptors;
+using System.Reflection;
+
+namespace eShop.Catalog.Types
+{
+    public static class UseToUpperObjectFieldDescriptorExtensions
+    {
+        public static IObjectFieldDescriptor UseToUpper(this IObjectFieldDescriptor descriptor)
+        {
+            return descriptor.Use(next => async context =>
+            {
+                await next(context);
+
+                if (context.Result is string s)
+                {
+                    context.Result = s.ToUpperInvariant();
+                }
+            });
+        }
+    }
+
+    public class UseToUpperAttribute : ObjectFieldDescriptorAttribute
+    {
+        protected override void OnConfigure(IDescriptorContext context, IObjectFieldDescriptor descriptor, MemberInfo info)
+        => descriptor.UseToUpper();
+    }
+}
